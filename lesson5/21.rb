7fbab520061e894fb3190032590e9f0cd26b9141
SarahBunker <ChemEnCoder@gmail.com>
2021-10-14 19:01:50 +0000

    in the middle of rubocop, want to make sure I have a commit with a working version.

--- lesson5/21.rb
+++ lesson5/21.rb
@@ -9,8 +9,8 @@ extras
 -------
 =end
 
-#breadcrumbs, working on a larger deck, fix array error
-#sometimes card was nil, I think I fixed it by adding bounds to where the cut card can be placed.
+# breadcrumbs, working on a larger deck, fix array error
+# sometimes card was nil, I think I fixed it by adding bounds cut card
 
 GOAL = 21
 NUM_DECKS = 4
@@ -24,54 +24,51 @@ class Hand
     @cards = []
     @total = nil
   end
-  
+
   def reset
     initialize
   end
-  
+
   def <<(card)
     cards << card
   end
-  
+
   def total_value
-    total = sum
+    sum
     while total > 21
-      break unless has_aces_as_elevens?
+      break unless aces_as_elevens?
       ace_to_one
     end
-    total = sum
+    total
   end
-  
+
   def show_hand
-    cards.each do |card|
-      card.show_card
-    end
+    cards.each(&:show_card)
   end
-  
+
   def show_hidden_cards
-    cards.each do |card|
-      card.show_hidden_card
-    end
+    cards.each(&:show_hidden_card)
     puts "The total is --- #{total_value}"
   end
-  
+
   protected
-  
+
   def <=>(other_hand)
     total_value <=> other_hand.total_value
   end
-  
+
   private
+
   attr_accessor :cards, :total
-  
+
   def sum
     s = 0
     cards.each do |card|
       s += card.value
     end
-    s
+    self.total = s
   end
-  
+
   def ace_to_one
     cards.each do |card|
       if card.ace? && card.eleven?
@@ -80,56 +77,55 @@ class Hand
       end
     end
   end
-  
-  def has_aces_as_elevens?
-    cards.any?{|card| card.eleven?}
+
+  def aces_as_elevens?
+    cards.any?(&:eleven?)
   end
-  
 end
 
 class Card
   attr_accessor :value
-  
+
   def initialize(name, suit)
     @name = name
     @suit = suit
     @hide = false
     @value = get_value
   end
-  
+
   def hide_card
     self.hide = true
   end
-  
+
   def eleven_to_one
     self.value = 1 if ace?
   end
-  
+
   def ace?
     name == "A"
   end
-  
+
   def eleven?
     value == 11
   end
-  
+
   def to_s
     "   The #{name} of #{suit}."
   end
-  
+
   def show_card
     puts self unless hidden?
     puts "   Face Down Card" if hidden?
   end
-  
+
   def show_hidden_card
     puts self
   end
-  
+
   private
   attr_writer :hide
   attr_reader :name, :suit
-  
+
   def get_value
     if (2..10).include?(name.to_i)
       return name.to_i
@@ -139,7 +135,7 @@ class Card
       return 10
     end
   end
-  
+
   def hidden?
     @hide
   end
@@ -147,15 +143,15 @@ end
 
 class Deck
   attr_accessor :deck, :known_cards
-  
+
   CARD_NAMES = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
   SUITS = %w(Spades Clubs Hearts Diamonds)
-  
+
   def initialize
     self.known_cards = []
     new_deck
   end
-  
+
   def new_deck
     array_cards = []
     self.known_cards = []
@@ -170,14 +166,14 @@ class Deck
     deck.shuffle!
     self.cut_card = cut_card_method
   end
-  
+
   def check_cards_left_in_deck
     if cut_card_reached?
       puts "The dealer reached the cut card during the last hand, so a new shuffled deck will be used."
       new_deck
     end
   end
-  
+
   def deal_a_card
     card = deck.pop
     known_cards << card
@@ -186,27 +182,27 @@ class Deck
     end
     card
   end
-  
+
   def deal_a_facedown_card
     card = deck.pop
     known_cards << card
     card.hide_card
     card
   end
-  
+
   private
-  
+
   attr_accessor :cut_card
 
   def cut_card_method
     min = max_cards_one_round(NUM_DECKS,NUM_PLAYERS)
     (min..deck.size - min).to_a.sample
   end
-  
+
   def cut_card_reached?
     deck.size <= cut_card
   end
-  
+
   def max_cards_one_round(num_decks, num_players)
     total = 0
     num_cards = 0
@@ -224,30 +220,30 @@ end
 
 class Participant
   attr_reader :hand
-  
+
   def initialize(deck)
     @hand = Hand.new
     @deck = deck
   end
-  
+
   def hit
     hand << deck.deal_a_card
   end
-  
+
   def busted?
     total > GOAL
   end
-  
+
   def total
     hand.total_value
   end
-  
+
   def new_card(card)
     hand << card
   end
-  
+
   private
-  
+
   attr_writer :hand
   attr_reader :deck
 end
@@ -262,12 +258,12 @@ end
 
 class Dealer < Participant
   HAND_DEALER_STOPS_AT = 17
-  
+
   def show_hand
     puts "The Dealer has the following cards:"
     hand.show_hand
   end
-  
+
   def strategy
     show_hidden_cards
     loop do
@@ -278,9 +274,9 @@ class Dealer < Participant
       show_hidden_cards
     end
   end
-  
+
   private
-  
+
   def show_hidden_cards
     puts "Dealer has these cards:"
     hand.show_hidden_cards
@@ -288,14 +284,13 @@ class Dealer < Participant
 end
 
 class Game21
-  
+
   def initialize
     @deck = Deck.new
     @player = Player.new(deck)
     @dealer = Dealer.new(deck)
-    
   end
-  
+
   def play
     welcome_message
     loop do
@@ -306,10 +301,10 @@ class Game21
     end
     goodbye_message
   end
-  
+
   private
   attr_reader :dealer, :player, :deck
-  
+
   def keep_playing?
     answer = nil
     loop do
@@ -321,7 +316,7 @@ class Game21
     puts "--------------------------"
     true unless answer == 'n'
   end
-  
+
   def round
     deal_cards
     show_cards
@@ -330,12 +325,12 @@ class Game21
     determine_winner
     puts "--------------------------"
   end
-  
+
   def reset_hands
     player.hand.reset
     dealer.hand.reset
   end
-  
+
   def welcome_message
     puts "Welcome to Twenty-One. Let's play"
     puts
@@ -344,18 +339,18 @@ class Game21
   def deal_cards
     player.new_card(deck.deal_a_card)
     player.new_card(deck.deal_a_card)
-    
+
     dealer.new_card(deck.deal_a_facedown_card)
     dealer.new_card(deck.deal_a_card)
   end
-  
+
   def show_cards
     dealer.show_hand
     puts
     player.show_hand
     puts
   end
-  
+
   def player_turn
     loop do
       answer = get_choice
@@ -367,7 +362,7 @@ class Game21
       break if player.busted?
     end
   end
-  
+
   def get_choice
     answer = nil
     loop do
@@ -378,12 +373,12 @@ class Game21
     end
     answer
   end
-  
+
   def dealers_turn
     dealer.strategy
     puts 
   end
-  
+
   def determine_winner
     if player.busted?
       puts "You busted."
@@ -401,7 +396,7 @@ class Game21
       end
     end
   end
-  
+
   def goodbye_message
     puts "Thanks for playing."
   end
@@ -409,11 +404,11 @@ class Game21
   def player_won
     puts "You won this game! The dealer's turn to pay."
   end
-  
+
   def dealer_won
     puts "The dealer won, cough up the money!"
   end
-  
+
   def noone_won
     puts "It's a tie. At least you don't have to pay."
   end
